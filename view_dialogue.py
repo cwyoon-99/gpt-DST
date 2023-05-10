02d0b3f834575c02e69d99331a6e5cff9dec7c8a
Seungyeon Kim <kairosial96@gmail.com>
2023-05-10 21:51:08 +0900

    230510_2150-temp

--- view_dialogue.py
+++ view_dialogue.py
@@ -1,12 +1,13 @@
 import json
 
-log_path = 'expts/230509_0236-gpt35_turbo_5p_v2_baseline_0to1842/running_log.json'
+dir_name = '230510_2051-gpt35_turbo_5p_v2_dialogue_context_0to74'
+log_path = 'expts/'+ dir_name +'/running_log.json'
 with open(log_path) as f:
     data = json.load(f)
 
 previous_id = ''
 turn_num = 0
-for turn in data[:33]:
+for turn in data[32:33]:
     current_id = turn['ID']
     if current_id != previous_id:
         print()
@@ -15,7 +16,7 @@ for turn in data[:33]:
     print('[system] ' + turn['dialog']['sys'][-1])
     print('[user] ' + turn['dialog']['usr'][-1])
     print('')
-    # print('prompt: ' + turn['prompt'])
+    print('prompt: ' + turn['prompt'])
     previous_id = current_id
     turn_num += 1
 print()
